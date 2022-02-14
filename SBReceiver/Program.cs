122e9f2bf9fc250a135cc511aa0afb5aec9c022d
Tim Kopp <timkopp@gmail.com>
2022-02-13 19:26:21 -0500

    Add project files.

---
+++ SBReceiver/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Azure.ServiceBus;
+using SBShared.Models;
+using System;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SBReceiver
+{
+    class Program
+    {
+        const string connstr = "";
+        const string qname = "personqueue";
+        static IQueueClient queueClient;
+        static async Task Main(string[] args)
+        {
+            queueClient = new QueueClient(connstr, qname); 
+            var messHandlerOptions = new MessageHandlerOptions(ExceptionHandler) {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+            queueClient.RegisterMessageHandler(ProcessMessagesAsync, messHandlerOptions);
+            Console.ReadLine();
+            await queueClient.CloseAsync();
+        }
+
+        private static async Task ProcessMessagesAsync(Message message, CancellationToken arg2)
+        {
+            var jsonstring = Encoding.UTF8.GetString(message.Body);
+            ReservationModel obj = JsonSerializer.Deserialize<ReservationModel>(jsonstring);
+            Console.WriteLine($"recieved { obj.ReservationName} { obj.ReservationIdentifier}");
+
+            await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        private static Task ExceptionHandler(ExceptionReceivedEventArgs arg)
+        {
+            Console.WriteLine(arg.Exception);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ SBShared/Models/ReservationModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SBShared.Models
+{
+    public class ReservationModel
+    {
+        [Required]
+        public string ReservationName { get; set; }
+        [Required]
+        public string ReservationIdentifier { get; set; }
+    }
+}
